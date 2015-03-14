f8c27f05ced1e99608dfca78782e6baed3a86549
Nikita Nikitin <berozzy@gmail.com>
2015-03-15 02:24:38 +0300

    /img/ → img/

--- lookbookItems.js
+++ lookbookItems.js
@@ -1,67 +1,67 @@
 window.lookbookItems = [{
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_12653r.jpg', '/img/lookbook/IMG_12r7999.jpg'],
+  pics: ['img/lookbook/IMG_12653r.jpg', 'img/lookbook/IMG_12r7999.jpg'],
   sizes: ["s", "m", "l"],
   price: 1500
 }, {
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_138rr22.jpg', '/img/lookbook/IMG_13r866.jpg'],
+  pics: ['img/lookbook/IMG_138rr22.jpg', 'img/lookbook/IMG_13r866.jpg'],
   sizes: ["s", "m", "l"],
   price: 1500
 }, {
   name: "Свитшот1",
   descr: "",
-  pics: ['/img/lookbook/IMG_14f68.jpg'],
+  pics: ['img/lookbook/IMG_14f68.jpg'],
   sizes: ["s", "m", "l"],
   price: 2800
 }, {
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_14j96.jpg'],
+  pics: ['img/lookbook/IMG_14j96.jpg'],
   sizes: ["s", "m", "l"],
   price: 1500
 }, {
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_143tt5gp.jpg'],
+  pics: ['img/lookbook/IMG_143tt5gp.jpg'],
   sizes: ["s", "m", "l"],
   price: 1500
 }, {
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_160j7.jpg', '/img/lookbook/IMG_160r2.jpg'],
+  pics: ['img/lookbook/IMG_160j7.jpg', 'img/lookbook/IMG_160r2.jpg'],
   sizes: ["s", "m", "l"],
   price: 1300
 }, {
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_1647j.jpg'],
+  pics: ['img/lookbook/IMG_1647j.jpg'],
   sizes: ["s", "m", "l"],
   price: 1500
 }, {
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_1544kk.jpg'],
+  pics: ['img/lookbook/IMG_1544kk.jpg'],
   sizes: ["s", "m", "l"],
   price: 1500
 }, {
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_1638h.jpg'],
+  pics: ['img/lookbook/IMG_1638h.jpg'],
   sizes: ["s", "m", "l"],
   price: 1500
 }, {
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_15p85p.jpg'],
+  pics: ['img/lookbook/IMG_15p85p.jpg'],
   sizes: ["s", "m", "l"],
   price: 1500
 }, {
   name: "Свитшот1",
   descr: "Сделано из настоящей пиздливой кожи Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, neque, voluptas, sequi, ipsa accusamus nostrum mollitia adipisci expedita ut amet enim asperiores corporis vitae iusto veniam vero voluptatem commodi ratione. Porro facilis blanditiis quos repellendus beatae ducimus laudantium dolor eius?",
-  pics: ['/img/lookbook/IMG_1p515.jpg', '/img/lookbook/IMG_165g8.jpg'],
+  pics: ['img/lookbook/IMG_1p515.jpg', 'img/lookbook/IMG_165g8.jpg'],
   sizes: ["s", "m", "l"],
   price: 1500
 }, ];
